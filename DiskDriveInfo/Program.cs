74570224839e86e24c46e52f44eb86a18389d22b
Yurchenko Denis <den4ik24@gmail.com>
2019-06-23 23:13:41 +0300

    инфо о дисках

---
+++ DiskDriveInfo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiskDriveInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(var drive in DriveInfo.GetDrives())
+            {
+                try
+                {
+                    Console.WriteLine("Имя диска: " + drive.Name);
+                    Console.WriteLine("Метка тома диска: " + drive.VolumeLabel);
+                    Console.WriteLine("корневой каталог диска: " + drive.RootDirectory);
+                    Console.WriteLine("Готовность диска: " + drive.IsReady);
+                    Console.WriteLine("Тип диска: " + drive.DriveType);
+                    Console.WriteLine("Файловая система: " + drive.DriveFormat);
+                    Console.WriteLine("Размер диска (Мб): " + drive.TotalSize / 1048576);
+                    Console.WriteLine("Общий объем свободного места доступного надиске (Мб): " + drive.TotalFreeSpace / 1048576);
+                    Console.WriteLine("Объем доступного свободного места (Мб): " + drive.AvailableFreeSpace / 1048576);
+                }
+                catch { }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
